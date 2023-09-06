475c8738c34ad99edd64ace9f36955b7b7cba9bc
jaifire <boydearea@163.com>
2023-09-06 15:07:03 +0800

    v0.1.4

--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,10 @@ Route::get('/', function () {
     lite_view('welcome.php');
 }, ['SayHello']);
 
+Route::get('config', function () {
+    dump(cfg());
+});
+
 Route::get('hello', [\App\Demo\Controllers\DemoController::class, 'hello']);
 Route::get('db', 'App\Demo\Controllers\DemoController@db');
 Route::post('log', 'App\Demo\Controllers\DemoController@log');
