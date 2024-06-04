b183c69af78ecd4eea4d3ff8edc00592efff68c0
jaifiresong <boydearea@163.com>
2024-06-04 22:19:29 +0800

    up logging.php

--- config/logging.php
+++ config/logging.php
@@ -4,9 +4,11 @@ use Monolog\Logger;
 
 return [
     "default" => [
-        "Handler" => \Monolog\Handler\RotatingFileHandler::class,
-        "path" => root_path("storage/logs/run.log"),
-        "level" => Logger::INFO,
+        "handlers" => [
+            new \Monolog\Handler\RotatingFileHandler(root_path("storage/logs/run.log"), Logger::INFO),
+            new \Monolog\Handler\StreamHandler('php://stdout', Logger::INFO),  // php://stdout OR php://stderr
+        ],
+        "format" => null,
         "processors" => [
             \Monolog\Processor\MemoryUsageProcessor::class
         ]
--- test.php
+++ test.php
@@ -5,5 +5,3 @@ require_once __DIR__ . '/vendor/autoload.php';
 
 
 var_dump(root_path());
-var_dump(cfg('app_name1'));
-var_dump(cfg('test'));
